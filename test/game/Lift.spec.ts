a703135a13617855f6538de865c24e7439e3cd10
javekk <periniraffaele@gmail.com>
2023-01-19 22:10:35 +0100

    More tests experiment

--- test/game/Lift.spec.ts
+++ test/game/Lift.spec.ts
@@ -12,5 +12,29 @@ describe("Lift class ", () => {
     it('should have pixelscale attribute set to 14', () => {
         expect(lift.pixelscale).toBe(14);
     });
+
+    it('should have scalesprite attribute set to 0.8035714', () => {
+        expect(lift.scalesprite).toBe(0.8035714);
+    });
+
+
+    describe('preload method should load the following images correctly', () => {
+       test('the background image', () => {
+            const spy = jest.spyOn(lift.load, 'image');
+            lift.preload();
+            expect(spy).toHaveBeenCalledWith('background', './assets/Lift - tiny.png');
+            spy.mockRestore();
+        });
+    });
+
+    test('preload should load the Mr. Bafo spritesheet', () => {
+        const spy = jest.spyOn(lift.load, 'spritesheet');
+        lift.preload();
+        expect(spy).toHaveBeenCalledWith('Mr.Bafo', 'assets/Mr.Bafo.png', {
+          frameWidth: 448,
+          frameHeight: 448,
+        });
+        spy.mockRestore();
+      });
 });
 
